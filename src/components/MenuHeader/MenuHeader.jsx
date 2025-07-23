aa3654c6a52ad9712c0ac94b9fcfc9accb4d4e41
LacamJC <joaoramajo744@gmail.com>
2025-07-23 14:27:20 -0300

    fix: ajusta rota dos links do novo menu

--- src/components/MenuHeader/MenuHeader.jsx
+++ src/components/MenuHeader/MenuHeader.jsx
@@ -1,12 +1,16 @@
+import { HashLink } from 'react-router-hash-link';
 import styles from './MenuHeader.module.css';
 function MenuHeader() {
     return (
         <>
             <nav className={`${styles.navbar} d-flex gap-3`}>
-                <a href="#" className={`${styles.navbar__item}`}>Sobre</a>
-                <a href="#" className={`${styles.navbar__item}`}>Serviços</a>
-                <a href="#" className={`${styles.navbar__item}`}>Contato</a>
-                <a href="#" className={`btn ${styles.button}`}>Entrar em contato</a>
+                <a href="/#sobre" className={`${styles.navbar__item}`}>Sobre</a>
+                <a href="/#atuacao" className={`${styles.navbar__item}`}>Áreas de Atuação</a>
+                <a href="/#contato" className={`${styles.navbar__item}`}>Contato</a>
+                <a href="/#contato" className={`btn ${styles.button}`}>Entrar em contato</a>
+                <HashLink smooth to="" className={`${styles.navbar__item}`}>
+
+                </HashLink>
             </nav>
         </>
     )
